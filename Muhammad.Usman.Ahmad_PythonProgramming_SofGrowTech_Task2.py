d3db998cca22a32e116518f76c39f87f733c63f4
mu6599600-creator <mu6599600@gmail.com>
2026-02-20 11:37:39 -0800

    Add files via upload

---
+++ Muhammad.Usman.Ahmad_PythonProgramming_SofGrowTech_Task2.py
@@ -0,0 +1,21 @@
+def chatbot():
+    print("Simple Chatbot Started!")
+    print("Type 'bye' to exit.\n")
+
+    while True:
+        user_input = input("You: ").lower()
+
+        if user_input == "hello" or user_input == "hi":
+            print("Bot: Hi!")
+
+        elif user_input == "how are you":
+            print("Bot: I'm fine, thanks!")
+
+        elif user_input == "bye":
+            print("Bot: Goodbye!")
+            break
+
+        else:
+            print("Bot: Sorry, I don't understand that.")
+
+chatbot()
